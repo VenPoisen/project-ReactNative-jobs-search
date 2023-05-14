15185abb05549eb3e08b6011a6c2b840a0bef465
Alan Martins <alanmf.pro@gmail.com>
2023-05-14 20:10:47 -0300

    Update Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -12,8 +12,11 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
 
   return (
     <View>
@@ -22,10 +25,44 @@ const Welcome = () => {
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
-      <View>
-        <View>
-          <TextInput style={styles.searchInput} />
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => { }}
+            placeholder="What are you looking for?"
+          />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)} >{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+
       </View>
     </View>
   )
